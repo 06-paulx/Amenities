f1cb8ff2425976e5b847d0e6e0a74a780978d7ee
francislo1221 <francislo1221@gmail.com>
2019-01-03 17:07:27 -0800

    edit css

--- client/src/components/Row1.jsx
+++ client/src/components/Row1.jsx
@@ -4,7 +4,7 @@ import Amenity from './Amenity.jsx'
 function Row1({amenities}){
   amenities = amenities.slice(0,6)
   return (
-    <div>
+    <div className="m-row">
       {
         amenities.map((amenity, index) => 
           <Amenity value={amenity} key={index}/>
